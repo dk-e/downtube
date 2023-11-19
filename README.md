7a91917ed9f3b6b321a815f7893456292d9fdc99
d <121973621+injectings@users.noreply.github.com>
2023-11-19 23:34:38 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -10,13 +10,14 @@ converts a youtube link to an audio (mp3) and/or video (mp4) file
 ####  1. Verify that you have pip and python installed => https://www.youtube.com/watch?v=ec5vjIg0bUA
 ####  2. Clone this repo - ```git clone https://github.com/injectings/downtube``` in cmd/pwsh.
 ####  3. Type ```cd downtube``` in cmd/pwsh.
-####  4. Run the command ```pip install requests colorama time os``` in cmd/pwsh.
+####  4. Run the command ```pip install pytube colorama os``` in cmd/pwsh.
 ####  5. Run the python script by double clicking on it or type: ```python downtube.py``` in cmd/pwsh.
 
-# advantages:
-####  1.  Advantage one
-####  2.  Advantage two
-####  3.  Advantage three
+# advantages
+####  1. Fast
+####  2. Bloat free
+####  3. Easy to use
+####  4. Looks good
 
 ## credits
 myself (zurly @injectings)
---
+++ downtube.py
@@ -0,0 +1,100 @@
+from pytube import YouTube
+import os
+from colorama import Fore, Style
+
+# Function to download mp3
+def download_mp3(url, destination_folder):
+    try:
+        yt = YouTube(url)
+        video = yt.streams.filter(only_audio=True).first()
+
+        # download the file
+        out_file = video.download(output_path=destination_folder)
+
+        # save the file as mp3
+        base, _ = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+
+        # Check if the file already exists
+        file_number = 1
+        while os.path.exists(new_file):
+            base, ext = os.path.splitext(out_file)
+            new_file = f"{base}_{file_number}.mp3"
+            file_number += 1
+
+        os.rename(out_file, new_file)
+
+        # Print title and views
+        print(Style.BRIGHT + Fore.WHITE + "Video Title: ", yt.title)
+        print(Style.BRIGHT + Fore.WHITE + "Video Views: ", yt.views)
+
+        if file_number > 1:
+            print(Fore.YELLOW + f"The filename '{os.path.basename(base)}.mp3' was already taken, so now it will be called '{os.path.basename(new_file)}'.")
+        
+        print(Style.BRIGHT + Fore.GREEN + yt.title + f" has been successfully downloaded as {os.path.basename(new_file)}.")
+    except Exception as e:
+        print(Fore.RED + f"An error occurred: {e}")
+
+# Function to download mp4
+def download_mp4(url, destination_folder):
+    try:
+        yt = YouTube(url)
+
+        # outputs the title and views of the yt vid selected
+        print(Style.BRIGHT + Fore.WHITE + "Video Title: ", yt.title)
+        print(Style.BRIGHT + Fore.WHITE + "Video Views: ", yt.views)
+
+        # downloads the highest resolution video
+        yd = yt.streams.get_highest_resolution()
+        out_file = yd.download(destination_folder)
+
+        # Check if the file already exists
+        file_number = 1
+        while os.path.exists(out_file):
+            base, ext = os.path.splitext(out_file)
+            new_file = f"{base}_{file_number}.mp4"
+            file_number += 1
+
+        os.rename(out_file, new_file)
+
+        if file_number > 1:
+            print(Fore.YELLOW + f"The filename '{os.path.basename(base)}.mp4' was already taken, so now it will be called '{os.path.basename(new_file)}'.")
+        
+        print(Style.BRIGHT + Fore.GREEN + yt.title + f" has been successfully downloaded as {os.path.basename(new_file)}.")
+    except Exception as e:
+        print(Fore.RED + f"An error occurred: {e}")
+
+# Main script
+if __name__ == "__main__":
+    print(f"""{Fore.CYAN}
+      _                     _         _          
+     | |                   | |       | |         
+   __| | _____      ___ __ | |_ _   _| |__   ___ 
+  / _` |/ _ \ \ /\ / / '_ \| __| | | | '_ \ / _ \.
+ | (_| | (_) \ V  V /| | | | |_| |_| | |_) |  __/
+  \__,_|\___/ \_/\_/ |_| |_|\__|\__,_|_.__/ \___|                                                                                              
+                                      -by zurly
+     """)
+
+    # Ask the user if they want to download as mp3 or mp4
+    choice = str(input(Fore.CYAN + "Do you want to download as mp3 or mp4 > ")).lower()
+
+    # Ask for the downloads folder name
+    folder_name = str(input(Fore.CYAN + "What do you want the downloads folder to be called > ")) or 'downloads'
+
+    # Check if the downloads folder already exists
+    downloads_folder = os.path.join(os.getcwd(), folder_name)
+    if not os.path.exists(downloads_folder):
+        os.makedirs(downloads_folder)
+    else:
+        print(Fore.YELLOW + f"The folder '{folder_name}' already exists. Using existing folder.")
+
+    # Ask for the YouTube URL
+    url = str(input(Fore.CYAN + "Enter youtube link > "))
+
+    if choice == 'mp3':
+        download_mp3(url, downloads_folder)
+    elif choice == 'mp4':
+        download_mp4(url, downloads_folder)
+    else:
+        print(Fore.RED + "Invalid choice. Please choose 'mp3' or 'mp4'.")
