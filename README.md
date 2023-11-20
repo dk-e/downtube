cf80f9d9c6b1c34ee34691a6ef7965c0c098f916
d <121973621+injectings@users.noreply.github.com>
2023-11-20 18:32:57 +0000

    ☁️ Option To Upload Files To Cloud
    This means that the user can get a raw file link to use where needed.

--- README.md
+++ README.md
@@ -4,20 +4,21 @@
 <h3 align="center">
 converts a youtube link to an audio (mp3) and/or video (mp4) file
 </h3>
-<img align="center" src="https://r2.e-z.host/1e931256-a4b9-4b37-b539-feff5e9e0a47/exsw61ti.png">
+<img align="center" src="https://r2.e-z.host/1e931256-a4b9-4b37-b539-feff5e9e0a47/00760es5.png">
 
 # how to use
 ####  1. Verify that you have pip and python installed => https://www.youtube.com/watch?v=ec5vjIg0bUA
 ####  2. Clone this repo - ```git clone https://github.com/injectings/downtube``` in cmd/pwsh.
 ####  3. Type ```cd downtube``` in cmd/pwsh.
-####  4. Run the command ```pip install pytube colorama os``` in cmd/pwsh.
+####  4. Run the command ```pip install pytube colorama os pyperclip requests``` in cmd/pwsh.
 ####  5. Run the python script by double clicking on it or type: ```python downtube.py``` in cmd/pwsh.
 
 # advantages
 ####  1. Fast
 ####  2. Bloat free
-####  3. Easy to use
-####  4. Looks good
+####  3. Option to upload to the cloud automatically
+####  4. Easy to use
+####  5. Looks good
 
 ## credits
 myself (zurly @injectings)
--- downtube.py
+++ downtube.py
@@ -1,9 +1,38 @@
-from pytube import YouTube
 import os
-from colorama import Fore, Back, Style
+from pytube import YouTube
+from colorama import Fore, Style
+import requests
+import pyperclip
+
+def upload_to_transfersh(file_path):
+    try:
+        max_file_size = 100 * 1024 * 1024  # 100 MB (adjust as needed)
+
+        # Check file size
+        file_size = os.path.getsize(file_path)
+        if file_size > max_file_size:
+            print(Fore.RED + f"File size exceeds the limit. Maximum allowed size is {max_file_size / (1024 * 1024)} MB.")
+            return None
+
+        with open(file_path, 'rb') as file:
+            response = requests.post('https://transfer.sh/', files={'file': file})
+            if response.status_code == 200:
+                link = response.text.strip()
+                # Copy the link to the clipboard
+                pyperclip.copy(link)
+                return link
+            else:
+                print(Fore.RED + f"Failed to upload to Transfer.sh. Status code: {response.status_code}, Response: {response.text}")
+                return None
+    except FileNotFoundError:
+        print(Fore.RED + "File not found. Please provide a valid file path.")
+        return None
+    except Exception as e:
+        print(Fore.RED + f"An error occurred while uploading to Transfer.sh: {e}")
+        return None
 
 # Function to download mp3
-def download_mp3(url, destination_folder):
+def download_mp3(url, destination_folder, upload_to_cloud=False):
     try:
         yt = YouTube(url)
         video = yt.streams.filter(only_audio=True).first()
@@ -25,24 +54,32 @@ def download_mp3(url, destination_folder):
         os.rename(out_file, new_file)
 
         # Print title and views
-        print(Style.BRIGHT + Fore.WHITE + "[🎬]Video Title: ", yt.title)
-        print(Style.BRIGHT + Fore.WHITE + "[🎬]Video Views: ", yt.views)
+        print("\n" + Style.BRIGHT + Fore.WHITE + "[🎬] Video Title:", yt.title)
+        print(Style.BRIGHT + Fore.WHITE + "[🎬] Video Views:", yt.views)
 
         if file_number > 1:
             print(Fore.YELLOW + f"The filename '{os.path.basename(base)}.mp3' was already taken, so now it will be called '{os.path.basename(new_file)}'.")
-        
-        print(Style.BRIGHT + Fore.GREEN + yt.title + f" has been successfully downloaded as '{os.path.basename(new_file)}'.")
+
+        if upload_to_cloud:
+            transfersh_link = upload_to_transfersh(new_file)
+            if transfersh_link:
+                print(Style.BRIGHT + Fore.GREEN + "File uploaded to Transfer.sh. Here is the link:", transfersh_link)
+                print(Fore.GREEN + "Link copied to clipboard. (Ctrl+V to paste)")
+            else:
+                print(Fore.RED + "Failed to upload to Transfer.sh.")
+        else:
+            print(Style.BRIGHT + Fore.GREEN + f"The video has been successfully downloaded as '{os.path.basename(new_file)}'.")
     except Exception as e:
         print(Fore.RED + f"An error occurred: {e}")
 
 # Function to download mp4
-def download_mp4(url, destination_folder):
+def download_mp4(url, destination_folder, upload_to_cloud=False):
     try:
         yt = YouTube(url)
 
         # Print title and views
-        print(Style.BRIGHT + Fore.WHITE + "[🎬]Video Title: ", yt.title)
-        print(Style.BRIGHT + Fore.WHITE + "[🎬]Video Views: ", yt.views)
+        print("\n" + Style.BRIGHT + Fore.WHITE + "[🎬] Video Title:", yt.title)
+        print(Style.BRIGHT + Fore.WHITE + "[🎬] Video Views:", yt.views)
 
         # downloads the highest resolution video
         yd = yt.streams.get_highest_resolution()
@@ -59,8 +96,16 @@ def download_mp4(url, destination_folder):
 
         if file_number > 1:
             print(Fore.YELLOW + f"The filename '{os.path.basename(base)}.mp4' was already taken, so now it will be called '{os.path.basename(new_file)}'.")
-        
-        print(Style.BRIGHT + Fore.GREEN + yt.title + f" has been successfully downloaded as '{os.path.basename(new_file)}'.")
+
+        if upload_to_cloud:
+            transfersh_link = upload_to_transfersh(new_file)
+            if transfersh_link:
+                print(Style.BRIGHT + Fore.GREEN + "File uploaded to Transfer.sh. Here is the link:", transfersh_link)
+                print(Fore.GREEN + "Link copied to clipboard. (Ctrl+V to paste)")
+            else:
+                print(Fore.RED + "Failed to upload to Transfer.sh.")
+        else:
+            print(Style.BRIGHT + Fore.GREEN + f"The video has been successfully downloaded as '{os.path.basename(new_file)}'.")
     except Exception as e:
         print(Fore.RED + f"An error occurred: {e}")
 
@@ -79,22 +124,29 @@ if __name__ == "__main__":
     # Ask the user if they want to download as mp3 or mp4
     choice = str(input(Fore.CYAN + "Do you want to download as mp3 or mp4 > ")).lower()
 
-    # Ask for the downloads folder name
-    folder_name = str(input(Fore.CYAN + "What do you want the downloads folder to be called > ")) or 'downloads'
+    # Ask the user if they want to upload to Transfer.sh
+    upload_to_cloud = input(Fore.CYAN + "Do you want to upload to Transfer.sh [to get a raw file link]? (yes/no) > ").lower() == 'yes'
 
-    # Check if the downloads folder already exists
-    downloads_folder = os.path.join(os.getcwd(), folder_name)
-    if not os.path.exists(downloads_folder):
-        os.makedirs(downloads_folder)
+    # Check if the user wants to upload to Transfer.sh and skip folder creation
+    if upload_to_cloud:
+        folder_name = 'temp-transfer-sh'
     else:
-        print(Fore.YELLOW + f"The folder '{folder_name}' already exists. Using existing folder.")
+        # Ask for the downloads folder name
+        folder_name = str(input(Fore.CYAN + "What do you want the downloads folder to be called > ")) or 'downloads'
+
+        # Check if the downloads folder already exists
+        downloads_folder = os.path.join(os.getcwd(), folder_name)
+        if not os.path.exists(downloads_folder):
+            os.makedirs(downloads_folder)
+        else:
+            print(Fore.YELLOW + f"The folder '{folder_name}' already exists. Using the existing folder.")
 
     # Ask for the YouTube URL
     url = str(input(Fore.CYAN + "Enter youtube link > "))
 
     if choice == 'mp3':
-        download_mp3(url, downloads_folder)
+        download_mp3(url, folder_name, upload_to_cloud)
     elif choice == 'mp4':
-        download_mp4(url, downloads_folder)
+        download_mp4(url, folder_name, upload_to_cloud)
     else:
         print(Fore.RED + "Invalid choice. Please choose 'mp3' or 'mp4'.")
